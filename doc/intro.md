e87a81f5f5fd2df7153fb5c0f213b6c2cb5e4a53
Siyuan Ma <smy20011@gmail.com>
2015-12-27 17:08:36 -0600

    Create clojure template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to anduino
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/anduino/core_test.clj
@@ -0,0 +1,7 @@
+(ns anduino.core-test
+  (:require [clojure.test :refer :all]
+            [anduino.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
