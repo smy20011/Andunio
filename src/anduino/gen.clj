0bcda11243fcdb8f078a4be77c07f68cf19e2285
Siyuan Ma <smy20011@gmail.com>
2015-12-28 23:44:51 -0600

    Add generater

---
+++ src/anduino/gen.clj
@@ -0,0 +1,29 @@
+(ns anduino.gen)
+
+(def count-if
+  (comp count filter))
+
+(defn attr-is?
+  [k v]
+  #(= v (k %)))
+
+(defn gen-state-proto
+  "Generate Arduino state layout"
+  [device]
+  (let [out-ports (filter (attr-is? :dir :input) (:ports device))]
+    {:bytes-count (count out-ports)
+     :layout      (map-indexed (fn [index item] {:offset index :name (:name item)})
+                               out-ports)
+     }))
+
+(defn gen-action-proto
+  "Generate proto"
+  ;; TODO: Finish action proto)
+
+(defn gen-proto
+  "Generate proto data (eg. data layout)"
+  [device]
+  {:state-proto (gen-state-proto device)
+   :action-proto (gen-action-proto device)})
+
+
