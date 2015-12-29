2abe54271bd96fe41558e8cd75834a0359b67a67
Siyuan Ma <smy20011@gmail.com>
2015-12-28 23:44:24 -0600

    Create proto dsl

--- src/anduino/core.clj
+++ src/anduino/core.clj
@@ -1,6 +1,33 @@
 (ns anduino.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn device
+  "Device definition"
+  [device-name & params]
+  (let [ports   (filter #(= :port (:type %)) params)
+        actions (filter #(= :action (:type %)) params)]
+  {:name     device-name
+   :ports    ports
+   :actions  actions}))
+
+(defn in-port
+  "Create an input port"
+  [port-name port-number]
+  {:type  :port
+   :dir   :input
+   :name   port-name
+   :port   port-number})
+
+(defn out-port
+  "Create a output port"
+  [port-name port-number]
+  {:type :port
+   :dir  :output
+   :name port-name
+   :port port-number})
+
+(defn action
+  "Create an action"
+  [action-name & params]
+  {:type  :action
+   :name   action-name
+   :params params})
--- test/anduino/core_test.clj
+++ test/anduino/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [anduino.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def sample-device 
+  (device "A device"
+          (in-port  "UserInput" 13)
+          (out-port "LedPort"   20)
+          (action "setLedInput" :input)))
+
+(deftest device-test
+  (testing "device"
+    (is (some? sample-device))
+    (is (= "A device" (:name sample-device)))
+    (is (= 2 (-> sample-device :ports count)))
+    (is (= 1 (-> sample-device :actions count)))
+    ))
